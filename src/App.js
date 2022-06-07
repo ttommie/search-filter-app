8a63a13040890205f59895def197c1f7b383d450
Tommie <devtommie@gmail.com>
2022-06-07 11:47:44 -0400

    Search filter app
    Working search form with updating list based on user input.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import data from './searchFilterData.json';
 
 function App() {
+  const [searchWord, setSearchWord] = useState('')
+
+  const filterWord = (val) => {
+    if (searchWord === '') {
+      return val;
+    } else if (val.first_name.toLowerCase().includes(searchWord.toLowerCase())) {
+      return val;
+    } 
+    return 0;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Tommie's Search Filter</h1>
+      <input type="text" placeholder="Search Name..." onChange={(e) => setSearchWord(e.target.value)}></input>
+      <h2>List of Names</h2>
+      {
+        data.filter((val) => {
+          return filterWord(val)
+        }).map((person, key) => {
+          return <p key={key}>{person.first_name}</p>
+        })
+      }
     </div>
   );
 }
